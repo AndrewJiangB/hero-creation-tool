7c6b2673cb9869a762d1aeeb03e189df23c68088
Alessio Carone <alessio.carone@travelport.com>
2021-07-02 21:10:37 +0200

    merge 5

--- src/module/tabs/backgroundStep.ts
+++ src/module/tabs/backgroundStep.ts
@@ -2,25 +2,24 @@
   Functions used exclusively on the Background tab
 */
 import { Step, StepEnum } from '../Step';
-
 class _BackgroundTab extends Step {
-  constructor() {
-    super(StepEnum.Background);
-  }
+    constructor() {
+        super(StepEnum.Background);
+    }
 
-  section = () => $('#backgroundDiv');
+    section = () => $('#backgroundDiv');
 
-  setListeners(): void {
-    /* IMPLEMENT AS NEEDED */
-  }
+    setListeners(): void {
+        /* IMPLEMENT AS NEEDED */
+    }
 
-  setSourceData(): void {
-    /* IMPLEMENT AS NEEDED */
-  }
+    setSourceData(): void {
+        /* IMPLEMENT AS NEEDED */
+    }
 
-  renderData(): void {
-    /* IMPLEMENT AS NEEDED */
-  }
+    renderData(): void {
+        /* IMPLEMENT AS NEEDED */
+    }
 }
 const BackgroundTab: Step = new _BackgroundTab();
 export default BackgroundTab;
--- src/module/utils.ts
+++ src/module/utils.ts
@@ -1,58 +1,60 @@
 export async function getItemListFromCompendiumByName(compendiumName: string) {
-  const pack = game.packs.get(compendiumName);
-  const items: Item[] = [];
-  for (const itemIndex of pack.index.keys()) {
-    items.push(pack.getDocument(itemIndex));
-  }
-  return await (await Promise.all(items)).map((i) => game.items.fromCompendium(i));
+    const pack = game.packs.get(compendiumName);
+    const items: Item[] = [];
+    for (const itemIndex of pack.index.keys()) {
+        items.push(pack.getDocument(itemIndex));
+    }
+    return await (await Promise.all(items)).map((i) => game.items.fromCompendium(i));
 }
 
 export async function getItemFromCompendiumByName(compendiumName: string, itemName: string) {
-  const pack = game.packs.get(compendiumName);
-  const index = pack.index.getName(itemName);
-  const item = await pack.getDocument(index._id);
-  return game.items.fromCompendium(item);
+    const pack = game.packs.get(compendiumName);
+    const index = pack.index.getName(itemName);
+    const item = await pack.getDocument(index._id);
+    return game.items.fromCompendium(item);
 }
 
 export async function getItemFromCompendiumByIndexId(compendiumName: string, itemId: string) {
-  const pack = game.packs.get(compendiumName);
-  const item = await pack.getDocument(itemId);
-  return game.items.fromCompendium(item);
+    const pack = game.packs.get(compendiumName);
+    const item = await pack.getDocument(itemId);
+    return game.items.fromCompendium(item);
 }
 
 export function getAbilityNameByKey(key: string) {
-  return key === 'any' ? game.i18n.localize(`HCT.Common.ANY`) : game.i18n.localize(`DND5E.Ability${key.capitalize()}`);
+    return key === 'any'
+        ? game.i18n.localize(`HCT.Common.ANY`)
+        : game.i18n.localize(`DND5E.Ability${key.capitalize()}`);
 }
 
 export function getAbilityModifierValue(value: number) {
-  return Math.floor((value - 10) / 2);
+    return Math.floor((value - 10) / 2);
 }
 
 export function modifierSign(val: number) {
-  return val >= 0 ? `+${val}` : `-${val}`;
+    return val >= 0 ? `+${val}` : `-${val}`;
 }
 
 export function openTab(id: string): void {
-  $('.hct-container .tabs').toggle(id !== 'startDiv'); // hides the tabs if switching to startDiv, else show them.
+    $('.hct-container .tabs').toggle(id !== 'startDiv'); // hides the tabs if switching to startDiv, else show them.
 
-  $('.tab-body').hide();
-  $('.tablinks').removeClass('active');
-  $(`[data-hct_tab=${id}]`).addClass('active');
-  $(`#${id}`).show();
+    $('.tab-body').hide();
+    $('.tablinks').removeClass('active');
+    $(`[data-hct_tab=${id}]`).addClass('active');
+    $(`#${id}`).show();
 }
 
 export function getValueFromInnerProperty(obj: any, key: string): any {
-  const keyParts: string[] = key.split('.');
-  if (keyParts.length == 1) {
-    return (obj as any)[keyParts[0]];
-  }
-  const currentKey: string = keyParts.shift() as string;
-  return getValueFromInnerProperty((obj as any)[currentKey], keyParts.join('.'));
+    const keyParts: string[] = key.split('.');
+    if (keyParts.length == 1) {
+        return (obj as any)[keyParts[0]];
+    }
+    const currentKey: string = keyParts.shift() as string;
+    return getValueFromInnerProperty((obj as any)[currentKey], keyParts.join('.'));
 }
 
 export function isCustomKey(keyList: any, value: string): boolean {
-  for (const key in keyList) {
-    if (keyList[key] === value) return false;
-  }
-  return true;
+    for (const key in keyList) {
+        if (keyList[key] === value) return false;
+    }
+    return true;
 }
